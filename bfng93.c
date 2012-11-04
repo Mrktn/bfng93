9486e3efa33a950be12e0476ed9f7fb6ae765495
Antoine Balestrat <merkil@savhon.org>
2012-11-04 12:23:33 +0100

    Updated email.

--- bfng93.c
+++ bfng93.c
@@ -1,6 +1,6 @@
 /*  bfng93
  *
- *  Copyright (c) 2012, Antoine Balestrat, merkil@savhon.org
+ *  Copyright (c) 2012, Antoine Balestrat, antoine.balestrat@gmail.com
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
